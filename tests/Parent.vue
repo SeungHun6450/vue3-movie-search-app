8cbeb38a394de8f522bc744fc4bbb0f1e0ff9e5a
SeungHun6450 <toffg6450@naver.com>
2022-05-29 23:58:07 +0900

    VTU

---
+++ tests/Parent.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Parent</h1>
+  <Child msg="HUN" />
+</template>
+
+<script>
+import Child from './Child'
+
+export default {
+  components: {
+    Child
+  }
+}
+
+</script>
--- tests/example.test.js
+++ tests/example.test.js
@@ -157,3 +157,30 @@ test('메세지를 변경합니다', async () => {
   expect(wrapper.vm.msg).toBe('Hello Hun!')
   expect(wrapper.find('div').text()).toBe('Hello Hun?')
 })
+
+// VTU API
+// mount
+// attrs: 특정 컴포넌트에 여러 속성들을 적용하는 용도로 사용한다.
+// data: 기존 컴포넌트가 가진 데이터를 덮어쓰고 테스트가 가능하다.
+// props: 
+// global: 여러 하위옵셥들을 사용할 수 있으며 예를 들어 우리가 사용했던 plugins(store,router,loadImage)가 있다.
+
+// methods
+// attributes
+// exits:특정 요소가 존재하는지 확인하는 용도이다.
+// find: 특정 요소를 선택자를 통해서 확인하는 용도이다.
+// setData: 비동기처럼 실행이 가능한 메소드이다. 컴포넌트 내부의 특정 데이터를 갱신해 줄 때 반응성 유지를 위해 사용한다.
+// unmount: 기존에 연결된 컴포넌트를 연결 해제를 위해 사용하는 용도이다.
+
+// property
+// vm: wrapper객체를 통해서 실제로 컴포넌트 내부에 있는 여러가지 데이터들, 메소드들을 활용할 때 this키워드 대신 사용한다. (view Model의 약어)
+
+// shallowMount
+// 컴포넌트를 연결하여 테스트 환경에서 사용할 수 있으며 컴포넌트를 연결한다는 개념은 mount같다.
+// Parent.vue, Child.vue에 예시를 작성했다.
+// shallow: 얕은
+// 테스트 환경에서 mock(가짜, 모의)로 출력이된다. 특정 컴포넌트에 붙어있는 다른 컴포넌트를 실제로 렌더링하지 않고 가짜로 렌더링을 한다.
+
+// mount: 실제 렌더링 이후 같이 출력, 구조가 복잡하면 전부 연결하여 출력하기 때문에 무거워진다.
+// shallowMount: 가짜로 만들어 연결을 하는 척 하여 테스트를 진행한다. shallowMount로 사용한 컴포넌트만 출력한다.
+// 핵심만을 테스트 하기 위해 되도록이면 shallowMount를 사용하자!
